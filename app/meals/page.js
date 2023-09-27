1d1f5f33561d2825b276f79f2975650e6bc55830
William <83734121+WilliamMcKenzie@users.noreply.github.com>
2023-09-27 22:39:10 +0000

    responsive ui

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,9 +1,11 @@
 'use client'
 import Image from 'next/image'
 import styles from '../modulestyle/meals.module.css'
+import cn from "classnames";
 import { config } from '@fortawesome/fontawesome-svg-core'
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useRef } from 'react';
 config.autoAddCss = false
 
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
@@ -18,28 +20,46 @@ const fetcher = (url, data) => {
 
 export default function Home() {
 
+    const recipeRef = useRef();
+
     const [recipes, setRecipes] = useState([]);
     const [recipeInput, setRecipeInput] = useState('');
-    const [nextLink, setNextLink] = useState([])
+    const [nextLink, setNextLink] = useState("")
     const [lastInput, setLastInput] = useState("")
 
+    const [recipeListClass, setRecipeListClass] = useState(cn(styles.recipes_list))
 
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries, observer) => {
+            const entry = entries[0];
+            if (entry.isIntersecting) setRecipeListClass(cn(styles.recipes_list))
+            else setRecipeListClass(cn(styles.recipes_list, styles.recipes_list_full_view))
+        });
+        observer.observe(recipeRef.current);
+    }, []);
 
     useEffect(() => {
 
         async function callProtein() {
-            const meals = await fetcher(`/api/meals?input=protein`, false)
-            setRecipes(meals.hits)
-            setNextLink(meals._links.next.href)
+            const meals = await fetcher(`/api/meals?input=protein%20meals`, false)
+            setRecipes(oldArr => [...oldArr, meals.hits]);
+            if (meals._links.next) {
+                setNextLink(meals._links.next.href)
+            }
+            else setNextLink(false)
         }
         callProtein()
         return () => { }
     }, [])
 
     const loadMore = async () => {
-        const meals = await fetcher(`/api/nextPage?url=${nextLink}&input=${lastInput}`, false)
-        setRecipes(meals.hits)
-        setNextLink(meals._links.next.href)
+        observer.observe(document.getElementById('0'));
+        if (lastInput && nextLink) {
+            const meals = await fetcher(`/api/nextPage?url=${nextLink}&input=${lastInput}`, false)
+            setRecipes(oldArr => [...oldArr, meals.hits]);
+            if (meals._links.next) setNextLink(meals._links.next.href)
+            else setNextLink(false)
+        }
     }
 
     return (
@@ -72,28 +92,39 @@ export default function Home() {
                     </input>
                     <FontAwesomeIcon className={styles.searchIcon} icon={faSearch} onClick={async () => {
                         const meals = await fetcher(`/api/meals?input=${recipeInput}`, false)
-                        console.log(meals.hits)
                         setLastInput(recipeInput)
-                        setRecipes(meals.hits)
+                        if (meals._links.next) setNextLink(meals._links.next.href)
+                        else setNextLink(false)
+                        setRecipes([])
+                        setRecipes(oldArr => [...oldArr, meals.hits]);
                     }} />
                     <FontAwesomeIcon className={styles.heartIcon} icon={faHeart} />
                 </div>
             </div>
-
-            <div className={styles.recipes_list}>
-                {recipes.length > 0 ? recipes.map((recipe, index) => (
-                    <div key={index} className={styles.recipe_card}>
-                        <img src={recipe.recipe.image}></img>
-                        <div className={styles.recipe_card_info}>
-                            {((recipe.recipe.dishType).toString()).toUpperCase()}
-                            <label>{recipe.recipe.label}</label>
-                            {(Math.round(parseInt(recipe.recipe.calories))) + " calories"}
-                        </div>
+            <div className={styles.recipes_container}>
+                <div className={recipeListClass}>
+                    <div className={styles.pages} ref={recipeRef}>
                     </div>
-                )) : <h1>Sorry !! No recipes found</h1>}
-            </div>
-            <div className={styles.pages}>
-                <button onClick={loadMore}>Load More</button>
+                    {recipes.length > 0 ? recipes.map((recipeList, index) => (
+
+                        <div className={styles.recipe_chunk} key={index} id={index.toString()}>
+                            {recipeList.map((curRecipe, index) => (
+                                <div key={index} className={styles.recipe_card}>
+                                    <img src={curRecipe.recipe.image}></img>
+                                    <div className={styles.recipe_card_info}>
+                                        {curRecipe.recipe.dishType ? ((curRecipe.recipe.dishType).toString()).toUpperCase() : ""}
+                                        <label>{curRecipe.recipe.label}</label>
+                                        {(Math.round(parseInt(curRecipe.recipe.calories))) + " calories & " + (Math.round(parseInt(curRecipe.recipe.totalNutrients.PROCNT.quantity))) + "g of protein"}
+                                    </div>
+                                </div>
+                            ))}
+                            <div />
+                        </div>
+                    )) : <></>}
+                    <div className={styles.pages}>
+                        {nextLink && lastInput ? <button onClick={loadMore}>Load More</button> : <></>}
+                    </div>
+                </div>
             </div>
         </main >
     )
